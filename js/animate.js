5a1c0646dfbdb4bbd2afa5c455b24e4341dd72bb
ramboo-raajesh <100186781+ramboo-raajesh@users.noreply.github.com>
2023-03-03 20:26:06 +0530

    Update animate.js

--- js/animate.js
+++ js/animate.js
@@ -7,7 +7,7 @@ var wow = new WOW(
   {
     boxClass:     'wow',      // animated element css class (default is wow)
     animateClass: 'animated', // animation css class (default is animated)
-    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    offset:       0,          // distance to the element when triggering the animation (default is zero)
     mobile:       false,       // trigger animations on mobile devices (default is true)
     live:         true,       // act on asynchronously loaded content (default is true)
     callback:     function(box) {
